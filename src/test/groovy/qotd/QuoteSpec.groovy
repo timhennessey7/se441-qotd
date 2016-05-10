2408f4b5a7ded595465e7a1556ae73db0d394c4a
tim <tim.hennessey7@gmail.com>
2016-05-09 21:58:13 -0500

    week 6

--- src/test/groovy/qotd/QuoteSpec.groovy
+++ src/test/groovy/qotd/QuoteSpec.groovy
@@ -15,8 +15,23 @@ class QuoteSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
+    def "test for valid quote text"() {
+		when: 'text is empty'
+		def p = new Quote(text: '')
+		
+		then: 'validation should fail'
+		!p.validate()	
+	}
+	
+	def "text not null"() {
+    	def p = new Quote(text: 'abc')
+		expect:
+		p.text != null
+	}
+	
+	def "text not empty"() {
+	    def p = new Quote(text: 'abc' )
+		expect:
+		p.text.length() > 0
+	}
 }
